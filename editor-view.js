1df4804c1d8be85ea110bfc9322a37e8bc9e61c9
Glitch (glitch-hello-website) <none>
2022-05-24 17:55:21 +0000

    🚤🤑 Checkpoint ./editor-view.js:21323441/1898

--- editor-view.js
+++ editor-view.js
@@ -1,6 +1,40 @@
-function render(domSpec) {
-  const [tagName,  = domSpec
+function renderSpec(spec) {
+  console.log(spec);
+  const [tagName, attrs, children] = spec;
   
+  const dom = document.createElement(tagName);
+  let contentDOM = undefined;
+  
+  for (const name of attrs) {
+    dom.setAttribute(name, attrs[name]);
+  }
+  
+  for (const child of children) {
+    if (child === 0) {
+      contentDOM = dom;
+    } else {
+      const renderedChild = render(child);
+      dom.appendChild(renderedChild.dom);
+      if (renderedChild.contentDOM) {
+        contentDOM = renderedChild.contentDOM;
+      }
+    }
+  }
+  
+  return { dom, contentDOM };
+}
+
+function render(node) {
+  const spec = node.type.toDOM(node);
+  const { dom, contentDOM } = renderSpec(spec);
+  
+  for (let i = 0 ; i < node.childCount ; i++) {
+    const child = node.child(0);
+    const childDOM = render(child);
+    dom.appendChild(childDOM);
+  }
+  
+  return dom;
 }
 
 class EditorView {
@@ -19,7 +53,13 @@ class EditorView {
   }
   
   dispatch(tr) {
-    this.state = this.state.apply(tr);
+    const newState = this.state.apply(tr);
+    this.updateState(newState);
+  }
+  
+  updateState(newState) {
+    this.state = newState;
+    this.dom.replaceChildren()
   }
 
   onBeforeInput(event) {
