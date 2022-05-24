1452a77c33638bc0b81516f4bb862984db9ae71c
Glitch (glitch-hello-website) <none>
2022-05-24 17:22:13 +0000

    👱👄 Checkpoint ./editor-view.js:21323441/1980

--- editor-view.js
+++ editor-view.js
@@ -2,15 +2,21 @@ class EditorView {
   constructor(dom) {
     this.dom = dom;
     this.onBeforeInput = this.onBeforeInput.bind(this);
-    this.dom.addEventListener('beforeinput', this.onBeforeInput);
+    this.dom.addEventListener("beforeinput", this.onBeforeInput);
     this.dom.contentEditable = true;
   }
-  
+
   destroy() {
-    this.dom.removeEventListener('beforeinput', this.onBeforeInput);
+    this.dom.removeEventListener("beforeinput", this.onBeforeInput);
   }
-  
+
   onBeforeInput(event) {
     event.preventDefault();
+
+    switch (event.type) {
+      case "insertText": {
+        alert(event.data);
+      }
+    }
   }
 }
