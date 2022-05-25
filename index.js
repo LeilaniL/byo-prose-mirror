88b850a30a0c04bfb331baed30946875c376c476
Glitch (glitch-hello-website) <none>
2022-05-25 19:15:24 +0000

    👮⚽️ Checkpoint ./index.js:21323441/5519

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 /* global EditorView */
+const { Schema } = require("prosemirror-model");
 const { EditorState } = require("prosemirror-state");
-const { schema } = require("prosemirror-schema-basic");
+const { schema: baseSchema } = require("prosemirror-schema-basic");
+const { addListNodes } = require("prosemirror-schema-list");
+
+const schema = new Schema({
+  nodes: addListNodes(baseSchema.spec.nodes, "paragraph block*", "block"),
+  marks: baseSchema.spec.marks,
+});
 
 window.view = new EditorView(document.querySelector("#editor"), {
-  state: EditorState.create({ schema }),
+  state: EditorState.create({
+    schema,
+    doc: schema.node("doc", null, [
+      schema.node("bullet_list", null, [
+        schema.node("list_item", null, [schema.node("paragraph")]),
+      ]),
+    ]),
+  }),
 });
