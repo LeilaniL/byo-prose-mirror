c700cb054481e8e442ef8a9f00c9a4484f05da99
Glitch (glitch-hello-website) <none>
2022-05-27 14:43:54 +0000

    🚥📉 Checkpoint ./index.js:21323441/1155

--- index.js
+++ index.js
@@ -2,6 +2,11 @@
 const { EditorState } = require("prosemirror-state");
 const { schema } = require("prosemirror-schema-basic");
 
+const doc = schema.node("doc", null, [
+  schema.node("paragraph", null, [schema.text("Hello, ProseMirror!")]),
+  schema.node("paragraph", null, [schema.text("Time to edit!")]),
+]);
+
 window.view = new EditorView(document.querySelector("#editor"), {
-  state: EditorState.create({ schema }),
+  state: EditorState.create({ doc }),
 });
