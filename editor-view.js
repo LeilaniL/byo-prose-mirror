d25e141409972e97c08f1590560fcf3b9ed52262
Glitch (glitch-hello-website) <none>
2023-10-20 19:53:24 +0000

    💺🐁 Checkpoint ./editor-view.js:60194924/606

--- editor-view.js
+++ editor-view.js
@@ -2,7 +2,39 @@ function renderNode(node) {
   if(node.isText) {
     return document.createTextNode(node.text);
   }  
+  const [tagName] = node.type.spec.toDOM(node);
+  return document.createElement(tagName);
 }
+
+class View {
+  constructor(node, dom, parent) {
+    this.node = node;
+    this.dom = dom;
+    this.parent = parent;
+  }
+  
+  destroy() {
+    this.parent = null;
+  }
+}
+
+class TextView extends View {};
+
+class NodeView extends View {
+  constructor(node, dom, parent) {
+    super(node, dom, parent);
+    this.children = [];
+  }
+  
+  destroy() {
+    super.destroy();
+    
+    for (const child of this.children) {
+      child.destroy();
+    }
+  }
+};
+
 
 class EditorView {
   constructor(dom, { state }) {
@@ -10,5 +42,9 @@ class EditorView {
     this.state = state;
 
     this.dom.contentEditable = true;
+  }
+  
+  destroy() {
+    super.destroy();
   }
 }
