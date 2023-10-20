d14a86ced7af8c841df21adbe9eddcdb44cbf702
Glitch (glitch-hello-website) <none>
2023-10-20 19:41:18 +0000

    🏓😎 Checkpoint ./index.js:60194924/3 ./index.html:60194924/368 ./editor-view.js:60194924/106

--- editor-view.js
+++ editor-view.js
@@ -1,3 +1,9 @@
+function renderNode(node) {
+  if(node.isText) {
+    return document.createTextNode(node.text);
+  }  
+}
+
 class EditorView {
   constructor(dom, { state }) {
     this.dom = dom;
